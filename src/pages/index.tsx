573767069e674b73db5251d2b5c86e74e69e55c5
Mattia Bonardi <mattiabonardi99@gmail.com>
2023-01-10 01:02:04 +0100

    Presentation and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
-import { Header } from '../components/Header'
+import { Header } from "../components/Header";
+import { Icon } from "../components/Icon";
+import styles from "../styles/pages/Index.module.css";
 
 export default function Home() {
   return (
     <>
       <Header></Header>
+      <div className={styles.titleWrapper}>
+        <h2 className={styles.subtitle}>I'm Mattia, a full stack</h2>
+        <h1 className={styles.title}>DEVELOPER</h1>
+      </div>
+      <footer className={styles.footer}>
+        <span className={styles.social}>
+          <Icon
+            iconName="instagram"
+            size="20"
+            onClickEvent={toInstagram}
+          ></Icon>
+        </span>
+        <span className={styles.social}>
+          <Icon iconName="github" size="20" onClickEvent={toGitHub}></Icon>
+        </span>
+        <span className={styles.social}>
+          <Icon iconName="linkedin" size="20" onClickEvent={toLinkedin}></Icon>
+        </span>
+        <span className={styles.social}>
+          <Icon iconName="mail" size="20" onClickEvent={toMail}></Icon>
+        </span>
+      </footer>
     </>
-  )
+  );
 }
+
+const toLinkedin = () =>
+  window.open(
+    "https://www.linkedin.com/in/mattia-bonardi-099b02235/",
+    "_blank"
+  );
+const toInstagram = () =>
+  window.open("https://www.instagram.com/mattiaabonardi/", "_blank");
+const toGitHub = () =>
+  window.open("https://github.com/mattiabonardi", "_blank");
+const toMail = () => window.open("mailto:mattiabonardi99@gmail.com");
