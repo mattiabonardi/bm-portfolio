8876a68fa0da08687a23edd85d743b3af2c13fc5
Mattia Bonardi <mattiabonardi99@gmail.com>
2023-01-28 21:14:41 +0100

    Removed work detail page

--- src/assets/works.ts
+++ src/assets/works.ts
@@ -2,7 +2,7 @@ import { WorkData } from "../types/work";
 
 export const workData: WorkData[] = [
   {
-    id: "tictactoe-ddd",
+    id: "tictactoe-3d",
     title: "Tic Tac Toe 3D",
     abstract:
       "Tic Tac Toe game in 3D mode served by full stack javascript framework.",
@@ -14,8 +14,8 @@ export const workData: WorkData[] = [
       },
       {
         icon: "info",
-        url: "/works/tictactoe-ddd/",
-        newWindow: false,
+        url: "http://localhost:8082/en/projects/tic-tac-toe-3d",
+        newWindow: true,
       },
       {
         icon: "demo",
@@ -24,35 +24,16 @@ export const workData: WorkData[] = [
       },
     ],
     mainTecnologies: ["typescript", "react", "three"],
-    content: {
-      intro:
-        "Tic Tac Toe 3D reproduces tic tac toe game in 3D mode. The project is based on Next.js and is written in Typescript, HTML and CSS. The UI is created using Mui Components and Three.js. The user plays against AI (min max algorithm) but it is still possibile win but it is still possible to win through a particular solution: thinking out of the box.",
-      architecture:
-        "Next.js is a full stack javascript framework based on Node.js for the server side and React.js for the client side. The framework offers a great solution to implement server side rendering in an optimal way with React.js. For each user event, the client side 3D component calls the AI served by server side API to get the optimal tic tac toe move.",
-      goal: "I decided to create this project to learn React.js, to practise with 3D model and to know the benefit of Next.js instead of Nuxt.js (Vue.js framework). ",
-      tecnologies: [
-        "typescript",
-        "react",
-        "three",
-        "node",
-        "css",
-        "html",
-        "docker"
-      ],
-      problems:
-        "The main difficulties I encountered concern the lack of knowledge of React.js and the handling of Three.js events.",
-    },
   },
   {
     id: "smeup-nc",
     title: "Smeup No Code",
-    abstract:
-      "No Code integration for Smeup UI",
+    abstract: "No Code integration for Smeup UI",
     links: [
       {
         icon: "info",
-        url: "/works/smeup-nc",
-        newWindow: false,
+        url: "http://localhost:8082/en/projects/smeup-nc",
+        newWindow: true,
       },
       {
         icon: "demo",
@@ -61,12 +42,5 @@ export const workData: WorkData[] = [
       },
     ],
     mainTecnologies: ["typescript", "react", "figma"],
-    content: {
-      intro: "Smeup NC is a Figma plugin which allows to generate Smeup UI DSL from Figma design file. The plugin is provided with Smeup Figma components library. Users can create mockups of Smeup ERP and in the end they can export the relative DSL.",
-      architecture: "The plugin consists of two projects: smeup-nc-core and smeup-nc-ui. The first converts Figma components into SCP_SCH (Smeup UI DSL) and the second is the plugin UI. Plugin UI retrieves the page layout and call the core to get the relative DSL converted.",
-      goal: "No code platforms are very powerful for non-technical professionals because they abstract the complexity. Smeup S.p.A has a dedicated application consultancy lob which has no technical figures. For Smeup consultants using a no-code platform is very useful. The personal goals of the project are increase my skills around event-driven programming, React.js and Node.js.",
-      problems: "The main problem I faced was converting from widget absolute layout to a special layout definiton in Smeup DSL based on based on row and column divisions (similar to Bootstrap).",
-      tecnologies: ["typescript", "react", "figma", "vite", "css", "html"],
-    },
   },
 ];
--- src/types/work.ts
+++ src/types/work.ts
@@ -4,19 +4,10 @@ export interface WorkData {
   abstract: string;
   mainTecnologies: string[];
   links: WorkLink[];
-  content: WorkContent
 }
 
 export interface WorkLink {
   icon: "github" | "demo" | "info";
   url: string;
   newWindow: boolean;
-}
-
-export interface WorkContent {
-  intro: string;
-  goal: string;
-  architecture: string;
-  problems: string;
-  tecnologies: string[]
 }
