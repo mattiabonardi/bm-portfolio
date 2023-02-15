ca535d37c562d809b32d4caef8a42a576795e0a4
Mattia Bonardi <mattiabonardi99@gmail.com>
2023-02-15 23:06:58 +0100

    Add application title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>BM Portfolio</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
